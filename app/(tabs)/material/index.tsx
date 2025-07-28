edde37aae9d30f923e07b440ca6ae5fef34b11bd
Juhkang <juhyunk0820@naver.com>
2025-07-28 22:38:01 +0900

    각 탭 및 화면 라우팅 로그인 라우팅 기능 : feat : 자료 화면 구조 설정 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/2

---
+++ app/(tabs)/material/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function MaterialHomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Material Redirecting</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
