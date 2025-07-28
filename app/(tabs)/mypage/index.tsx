83b17ba829a984a99c4808a6b4c79741b753e757
Juhkang <juhyunk0820@naver.com>
2025-07-28 22:38:22 +0900

    각 탭 및 화면 라우팅 로그인 라우팅 기능 : feat : 마이페이지 화면 구조 설정 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/2

---
+++ app/(tabs)/mypage/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function MypageHomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Mypage Redirecting</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
