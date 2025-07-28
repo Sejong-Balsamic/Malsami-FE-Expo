d3dd1a477cc803eb619664750d54ba0eb75b8496
Juhkang <juhyunk0820@naver.com>
2025-07-28 13:48:11 +0900

    로그인 및 정보 활용 api 기능 추가 : feat : axios 관련 base 설정 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/1

--- api/axios.ts
+++ api/axios.ts
@@ -1,8 +1,25 @@
-import axios from "axios";
+import { storageKeys } from "@/constants";
+import { getSecureStore } from "@/utils";
+import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_SERVER_BASEURL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
 });
+
+// axiosInstance.interceptors.request.use(
+//   async (config: InternalAxiosRequestConfig) => {
+//     const token = await getSecureStore(storageKeys.ACCESS_TOKEN);
+//     if (token) {
+//       config.headers = config.headers ?? {};
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
 export { axiosInstance };
