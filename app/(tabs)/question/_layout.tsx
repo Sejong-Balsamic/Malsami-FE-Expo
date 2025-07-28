e5ce6a3d43fb6478fe91956ab6863d2182b086cd
Juhkang <juhyunk0820@naver.com>
2025-07-28 22:39:03 +0900

    각 탭 및 화면 라우팅 로그인 라우팅 기능 : feat : 질문 화면 구조 설정 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/2

---
+++ app/(tabs)/question/_layout.tsx
@@ -0,0 +1,50 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Fontisto from "@expo/vector-icons/Fontisto";
+
+export default function QuestionLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          headerLeft: () => (
+            <View style={styles.headerLeftContainer}>
+              <Image
+                style={styles.image}
+                resizeMode="cover"
+                source={require("@/assets/images/logo/malsamiLogo.jpeg")}
+              />
+              <Text style={styles.logoText}>malsami</Text>,
+            </View>
+          ),
+          headerRight: () => <Fontisto name="bell" size={24} color="black" />,
+        }}
+      />
+    </Stack>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerLeftContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    gap: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 24,
+    height: 24,
+  },
+  logoText: {
+    fontSize: 24,
+  },
+});
---
+++ app/(tabs)/question/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function QuestionHomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Question Redirecting</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
