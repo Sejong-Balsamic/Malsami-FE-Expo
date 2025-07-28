1610dc413e810f5523154269c89696ebd7a0c738
Juhkang <juhyunk0820@naver.com>
2025-07-28 22:44:44 +0900

    각 탭 및 화면 라우팅 로그인 라우팅 기능 : feat : 메인 홈 화면 구현, 공용 InputField 호출, 캐릭터 호출 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,54 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import InputField from "@/components/InputField";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import Character from "@/components/Character";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchHeaderContainer}>
+          <Character />
+          <Text>반가워요, 강주현님!{`\n`}궁금한 자료를 검색해보세요!</Text>
+        </View>
+        <View style={styles.searchHeaderInputContainer}>
+          <InputField
+            placeHolderValue="검색할 것을 입력"
+            leftChild={<EvilIcons name="search" />}
+            type="search"
+          />
+        </View>
+      </View>
+      <Text>Home Redirecting</Text>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  searchContainer: {},
+  searchHeaderContainer: {
+    flexDirection: "row",
+    zIndex: 10,
+    alignItems: "center",
     gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  searchHeaderImage: {
+    width: 120,
+    height: 180,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  searchHeaderInputContainer: {
+    marginTop: -40,
+    zIndex: 10,
   },
 });
