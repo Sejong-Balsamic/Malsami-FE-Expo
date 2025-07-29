d1e134b3f4794cd8c18a13f884d50cd58bedce03
Juhkang <juhyunk0820@naver.com>
2025-07-29 23:52:38 +0900

    회원가입 화면 라우팅 진행중

---
+++ app/auth/index.tsx
@@ -0,0 +1,11 @@
+import AuthRouteModal from "@/components/AuthRouteModal";
+import { router } from "expo-router";
+import { SafeAreaView, Text } from "react-native";
+
+export default function AuthScreen() {
+  return (
+    <SafeAreaView>
+      <Text>로그인 화면</Text>
+    </SafeAreaView>
+  );
+}
