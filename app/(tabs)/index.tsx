a1b0ebd76a5f3c9513291e90903ae8f2d5723715
Juhkang <juhyunk0820@naver.com>
2025-08-08 23:47:51 +0900

    디자인 변경에 따른 기존 화면 초기화 작업

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,8 @@
-import InputField from "@/components/InputField";
-import {
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import EvilIcons from "@expo/vector-icons/EvilIcons";
-import Character from "@/components/Character";
+import { ScrollView, StyleSheet, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-      <View style={styles.searchContainer}>
-        <View style={styles.searchHeaderContainer}>
-          <Character />
-          <Text>반가워요, 강주현님!{`\n`}궁금한 자료를 검색해보세요!</Text>
-        </View>
-        <View style={styles.searchHeaderInputContainer}>
-          <InputField
-            placeHolderValue="검색할 것을 입력"
-            leftChild={<EvilIcons name="search" />}
-            type="search"
-          />
-        </View>
-      </View>
       <Text>Home Redirecting</Text>
     </ScrollView>
   );
