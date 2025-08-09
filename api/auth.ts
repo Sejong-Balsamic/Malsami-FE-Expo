5591a43f7ed6f6ed66a96fa93a27b036ec3f42ea
Juhkang <juhyunk0820@naver.com>
2025-08-09 19:42:18 +0900

    api 사용 및 hook 사용 별 타입 재설정 : feat : getInfo타입 MemberDto 적용 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/34

--- api/auth.ts
+++ api/auth.ts
@@ -1,15 +1,21 @@
 import { axiosInstance } from "@/api";
 import { storageKeys } from "@/constants";
 import { AuthDto } from "@/types/responses/authDto";
+import { MemberDto } from "@/types/responses/memberDto";
+import { setHeader } from "@/utils";
 import { getSecureStore } from "@/utils/secureStore";
 
 async function postSignin(body: FormData): Promise<AuthDto> {
+  console.log(body);
   const { data } = await axiosInstance.post("api/auth/mobile/signin", body);
-
+  console.log(data);
   return data;
 }
 
-async function postGetUserInfo(): Promise<AuthDto> {
+async function postGetUserInfo(): Promise<MemberDto> {
+  const accessToken = await getSecureStore(storageKeys.ACCESS_TOKEN);
+  console.log("accessToken", accessToken);
+  setHeader("Authorization", `Bearer ${String(accessToken)}`);
   const { data } = await axiosInstance.post("/api/member/my-info");
 
   return data;
