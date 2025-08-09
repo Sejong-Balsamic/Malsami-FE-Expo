741beea608e24d95401c40dd15dffc08df2b8a8e
Juhkang <juhyunk0820@naver.com>
2025-08-09 19:53:52 +0900

    util 함수 구현 및 활용 : feat : axios 함수 편의성 개선작업: interceptor를 사용한 로그 출력 및 헤더 타입 설정 전역화 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/4

--- api/axios.ts
+++ api/axios.ts
@@ -8,4 +8,27 @@ const axiosInstance = axios.create({
   },
 });
 
+axiosInstance.interceptors.request.use(
+  (request) => {
+    console.log("Axios Request:", request);
+    return request;
+  },
+  (error) => {
+    console.error("Axios Request Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log("Axios Response:", response);
+    return response;
+  },
+  (error) => {
+    console.error("Axios Response Error:", error);
+    console.error(error.status);
+    return Promise.reject(error);
+  }
+);
+
 export { axiosInstance };
