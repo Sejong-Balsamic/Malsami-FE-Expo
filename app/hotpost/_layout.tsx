4348208ccf311207d3dbbe9110cb04086f7eaaf6
Juhkang <juhyunk0820@naver.com>
2025-08-26 01:33:32 +0900

    일간주간 탭 리스트화 페이지 및 상세 페이지 : feat : hot인기자료 부분 상세페이지 라우팅 요소 추가 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/57

---
+++ app/hotpost/_layout.tsx
@@ -0,0 +1,44 @@
+import { colors } from "@/constants";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { router, Stack } from "expo-router";
+import { Pressable } from "react-native";
+
+export default function HotPostLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: colors.UNCHANGED_WHITE,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          headerTitle: "HOT 인기자료",
+          headerTitleStyle: {
+            color: colors.UNCHANGED_BLACK,
+            fontSize: 18,
+            fontWeight: "600",
+          },
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <Pressable onPress={() => router.replace("/")}>
+              <AntDesign name="left" size={24} color={colors.UNCHANGED_BLACK} />
+            </Pressable>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="[id]"
+        options={{
+          headerShown: true,
+          headerTitle: "HOT 인기자료",
+          headerBackButtonDisplayMode: "minimal",
+        }}
+      />
+    </Stack>
+  );
+}
