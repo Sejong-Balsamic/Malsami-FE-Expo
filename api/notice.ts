4348c9295f7011825cfc77e65f0bf9b35cf9b185
Juhkang <juhyunk0820@naver.com>
2025-09-02 23:48:50 +0900

    notice 관련 api

--- api/notice.ts
+++ api/notice.ts
@@ -1,11 +1,22 @@
 import { NoticePostDto } from "@/types/responses/noticePostDto";
 import { axiosInstance } from "./axios";
+import { NoticePostCommand } from "@/types/requests/noticePostCommand";
+import { NoticePost } from "@/types/entities/postgres/noticePost";
 
 async function getNoticePosts(): Promise<NoticePostDto> {
   const body = new FormData();
   body.append("page", "1");
   const { data } = await axiosInstance.post("/api/notice/get/pinned", body);
+
   return data;
 }
 
-export { getNoticePosts };
+async function getFilteredNoticePosts(filterText: string): Promise<NoticePost> {
+  const formData = new FormData();
+  formData.append("query", filterText);
+  const { data } = await axiosInstance.post("/api/notice/filter", formData);
+
+  return data;
+}
+
+export { getNoticePosts, getFilteredNoticePosts };
