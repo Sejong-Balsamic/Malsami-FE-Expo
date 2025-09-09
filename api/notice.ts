d18a51c5c60c32d976a29eda1fe525bbc0b4a4c3
Juhkang <juhyunk0820@naver.com>
2025-09-10 01:18:34 +0900

    공지사항 섹션 구현 : feat : notice관련 api 수정 작업: 기존 api 삭제 및 filter 적용 api 추가 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/54

--- api/notice.ts
+++ api/notice.ts
@@ -1,22 +1,11 @@
 import { NoticePostDto } from "@/types/responses/noticePostDto";
 import { axiosInstance } from "./axios";
-import { NoticePostCommand } from "@/types/requests/noticePostCommand";
-import { NoticePost } from "@/types/entities/postgres/noticePost";
 
-async function getNoticePosts(): Promise<NoticePostDto> {
+async function getNoticeFilter(): Promise<NoticePostDto> {
   const body = new FormData();
-  body.append("page", "1");
-  const { data } = await axiosInstance.post("/api/notice/get/pinned", body);
-
+  body.append("dummy", "value");
+  const { data } = await axiosInstance.post("/api/notice/filter", body);
   return data;
 }
 
-async function getFilteredNoticePosts(filterText: string): Promise<NoticePost> {
-  const formData = new FormData();
-  formData.append("query", filterText);
-  const { data } = await axiosInstance.post("/api/notice/filter", formData);
-
-  return data;
-}
-
-export { getNoticePosts, getFilteredNoticePosts };
+export { getNoticeFilter };
