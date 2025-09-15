6eebccfa76e6b7a22f4d51bba4c440b9cf2f8cbf
Juhkang <juhyunk0820@naver.com>
2025-09-15 23:29:58 +0900

    UIUX 기능 향상 리팩토링 : feat : 메인화면 padding값 조정 및 사용되지 않는 값들 제거 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/70

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -62,7 +62,7 @@ export default function HomeScreen() {
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}
       >
-        <View>
+        <View style={{ paddingHorizontal: 20 }}>
           <WelcomeSection
             authData={auth}
             onPressLogin={() => router.replace("/auth")}
@@ -165,21 +165,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-  },
-  searchContainer: {},
-  searchHeaderContainer: {
-    flexDirection: "row",
-    zIndex: 10,
-    alignItems: "center",
-    gap: 8,
-  },
-  searchHeaderImage: {
-    width: 120,
-    height: 180,
-  },
-  searchHeaderInputContainer: {
-    marginTop: -40,
-    zIndex: 10,
+    paddingVertical: 20,
   },
 });
