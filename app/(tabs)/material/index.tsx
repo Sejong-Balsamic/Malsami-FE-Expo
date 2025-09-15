fa8689f7beb9bd9cf1a0a56b0cd412ce94871bc4
Juhkang <juhyunk0820@naver.com>
2025-09-15 23:37:37 +0900

    검색 입력 필드 및 검색 페이지모달 개선 : feat : Document탭 메인 화면 - SearchModalBox 사용 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/67

--- app/(tabs)/material/index.tsx
+++ app/(tabs)/material/index.tsx
@@ -1,11 +1,41 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import SearchModalBox from "@/components/screen/tab/SearchModalBox";
+import { colors } from "@/constants";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function MaterialHomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Material Redirecting</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerText}>
+        필요한 자료를{" "}
+        <Text style={{ color: colors.PRIMARY_DOCUMENT_COLOR }}>구매</Text>
+        하고,{`\n`}여러 자료를{" "}
+        <Text style={{ color: colors.PRIMARY_DOCUMENT_COLOR }}>
+          직접 올려보세요!
+        </Text>
+      </Text>
+      <View style={styles.searchContainer}>
+        <SearchModalBox type="document" onPress={() => {}} />
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: colors.UNCHANGED_WHITE,
+  },
+  headerText: {
+    paddingHorizontal: 20,
+    lineHeight: 28,
+    marginTop: 40,
+    fontSize: 20,
+    fontWeight: "500",
+    color: colors.UNCHANGED_BLACK,
+  },
+  searchContainer: {
+    paddingHorizontal: 20,
+    marginTop: 24,
+  },
+});
