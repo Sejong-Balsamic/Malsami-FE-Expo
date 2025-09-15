30130e2bdbce4f3233a333f25e888ece4334c10f
Juhkang <juhyunk0820@naver.com>
2025-09-15 23:38:05 +0900

    검색 입력 필드 및 검색 페이지모달 개선 : feat : Question 탭 메인 화면 - SearchModalBox 사용 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/67

--- app/(tabs)/question/index.tsx
+++ app/(tabs)/question/index.tsx
@@ -1,11 +1,40 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import SearchModalBox from "@/components/screen/tab/SearchModalBox";
+import { colors } from "@/constants";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 
 export default function QuestionHomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Question Redirecting</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerText}>
+        궁금한 내용을{" "}
+        <Text style={{ color: colors.PRIMARY_QUESTION_COLOR }}>질문</Text>
+        하고,{`\n`}다른 사람의 질문에{" "}
+        <Text style={{ color: colors.PRIMARY_QUESTION_COLOR }}>답변</Text>해
+        주세요!
+      </Text>
+      <View style={styles.searchContainer}>
+        <SearchModalBox type="question" onPress={() => {}} />
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.UNCHANGED_WHITE,
+  },
+  headerText: {
+    paddingHorizontal: 20,
+    lineHeight: 28,
+    marginTop: 40,
+    fontSize: 20,
+    fontWeight: "500",
+    color: colors.UNCHANGED_BLACK,
+  },
+
+  searchContainer: {
+    paddingHorizontal: 20,
+    marginTop: 24,
+  },
+});
