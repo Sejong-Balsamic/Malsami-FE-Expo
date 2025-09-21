de499e2886a65c39bf2019d694498cf1eadd243a
Juhkang <juhyunk0820@naver.com>
2025-09-21 21:36:49 +0900

    검색 입력 필드 및 검색 페이지모달 개선 : feat : 최근검색 태그 컴포넌트 수정: handler 설정 https://github.com/Sejong-Balsamic/Malsami-FE-Expo/issues/67

--- components/screen/tab/RecentSearchTags.tsx
+++ components/screen/tab/RecentSearchTags.tsx
@@ -28,19 +28,7 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     paddingHorizontal: 12,
     paddingVertical: 8,
-    // shadowColor: "#0000001A",
-    // shadowOpacity: 0,
-    // shadowRadius: 10,
-    // shadowOffset: { width: 1, height: 2 },
-    // elevation: 2,
     boxShadow: "1px 2px 10px 0px #0000001A",
-    // // iOS 그림자 (box-shadow: 1px 2px 10px 0px #0000001A)
-    // shadowColor: "#000000",
-    // shadowOpacity: 0.1, // #0000001A의 투명도 (26/255 ≈ 0.1)
-    // shadowRadius: 10, // blur radius
-    // shadowOffset: { width: 1, height: 2 }, // x: 1px, y: 2px
-    // // Android 그림자
-    // elevation: 3,
   },
   tagText: {
     fontSize: 14,
